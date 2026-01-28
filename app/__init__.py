d3b6762890735e9c77c2cbfa3433020dd51f8471
black shadow <nirmalsujit541@gmail.com>
2026-01-29 00:37:50 +0530

    Setting up configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
+from app.config import config
 
 
 def create_app():
     app = Flask(__name__)
+    
+    env = os.environ.get('FLASK_ENV', 'development')
+    app.config.from_object(config[env])
     
     return app
