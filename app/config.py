1491471bddc080b2d26b9e01d05b61f6cc4485a0
black shadow <nirmalsujit541@gmail.com>
2026-01-29 00:41:07 +0530

    Setting up configuration

--- app/config.py
+++ app/config.py
@@ -2,7 +2,6 @@ import os
 
 
 class BaseConfig:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     MONGODB_URI = os.environ.get('MONGODB_URI')
     MONGODB_DB = os.environ.get('MONGODB_DB')
     GITHUB_WEBHOOK_SECRET = os.environ.get('GITHUB_WEBHOOK_SECRET')
@@ -10,12 +9,14 @@ class BaseConfig:
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     MONGODB_URI = os.environ.get('MONGODB_URI') or 'mongodb://localhost:27017/'
     MONGODB_DB = os.environ.get('MONGODB_DB') or 'webhook_dev'
 
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 config = {
