a69bdfe7fc39a055349726638080f2159c51c717
black shadow <nirmalsujit541@gmail.com>
2026-01-29 00:44:52 +0530

    Fixed the initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask
-from app.config import config
+from app.config import DevelopmentConfig, ProductionConfig
 
 
 def create_app():
     app = Flask(__name__)
     
     env = os.environ.get('FLASK_ENV', 'development')
-    config_class = config.get(env, config['development'])
+    if env == 'production':
+        config_class = ProductionConfig
+    else:
+        config_class = DevelopmentConfig
+    
     app.config.from_object(config_class)
     
     return app
